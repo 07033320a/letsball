5aaf0fc389384781374dd600364021ce8374faba
Rex <503431920@qq.com>
2015-10-13 13:14:33 +0800

    update load function

--- src/main/webapp/WEB-INF/js/load.js
+++ src/main/webapp/WEB-INF/js/load.js
@@ -2,7 +2,27 @@
 var LOAD = {};
 /** 域对象 END */
 
+LOAD.flag = true;
+LOAD.message = "";
+
 /** 加载HTML */
-LOAD.loadHTML = function(htmlName) {
-	$("#page-wrapper").load(htmlName + ".html");
+LOAD.loadHTML = function(parent, htmlName) {
+	console.log(parent, htmlName);
+	if(parent == null || parent == undefined) {
+		parent = "page-wrapper";
+	}
+	if(htmlName == null || htmlName == undefined) {
+		LOAD.flag = false;
+		LOAD.message = "htmlName is null";
+		return false;
+	}
+	$("#" + parent).load(htmlName + ".html");
+};
+
+/** 加载HTML带参数的 */
+LOAD.loadHTMLParams = function(parent, htmlName, json) {
+	LOAD.loadHTML(parent, htmlName);
+	if(LOAD.flag == true) {
+		sessionStorage.setItem("jsonVal", json);
+	}
 };
